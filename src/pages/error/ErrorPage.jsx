c3083d166e726f9294ed4ceea345ed278c8361c4
apurvasankhe1338 <apurvaasankhe1338@gmail.com>
2025-02-08 16:52:44 +0530

    Add files via upload

---
+++ src/pages/error/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='flex justify-center items-center '>
+            <h2 className='text-5xl font-extrabold text-red-600'>Error</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
